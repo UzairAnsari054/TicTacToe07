fc01be8eaf9bacf6992a9f9bbbe6950110fc9a76
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-02-27 09:33:34 +0530

    Final Commit

---
+++ src/components/Icons.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { IconProps } from 'react-native-vector-icons/Icon';
+
+type IconsProps = PropsWithChildren<{
+    name: string;
+}>
+
+const Icons = ({name}: IconProps) => {
+  switch(name){
+    case 'circle':
+        return <Icon name="circle-thin" size={36} color="#F7CD2E" />
+        break
+    case 'cross':
+        return <Icon name="times" size={36} color="#38CC77" />
+        break
+
+        default:
+            return <Icon name="pencil" size={36} color="#0D0D0D" />
+  }
+}
+
+export default Icons
